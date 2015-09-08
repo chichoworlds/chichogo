271fa9e67c1aa2f34fdb0ef93eaca79bfa1c41e5
Fede Crespo <fedecrespo90@gmail.com>
2015-09-08 06:37:20 -0300

    Create single.html

---
+++ template/mapa/index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>kevin</title>
+</head>
+<body>
+	<iframe style="border-style:none" src="map.php" width="607" height="422"></iframe>	
+</body>
+</html>
